8c449d36f9869208518b22c1d9b86758bf650ccf
Kevin Elvio <62099198+KevinElvio@users.noreply.github.com>
2024-12-10 11:07:50 +0700

    Feat: Add Cors

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ const express = require('express')
 const UserRoutes = require('./routes/usersRoute')
 const app = express()
 const PORT = process.env.PORT || 5000
-
+const cors = require("cors");
 
 app.use(express.json())
 
+app.use(cors());
 app.use('/', UserRoutes)
 
 app.listen(PORT, () => {
